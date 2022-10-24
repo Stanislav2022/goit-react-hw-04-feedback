d3d918dd8211f581499cf76176b885b7657f6706
Stanislav2022 <tsv5@ukr.net>
2022-10-24 22:49:16 +0300

    1

---
+++ src/components/FeedbackWidget/Notification.jsx
@@ -0,0 +1,10 @@
+import PropTypes from 'prop-types'
+
+export default function Notification({text}) {
+ return (
+        <p>{text}</p>
+  )
+}
+Notification.propTypes = {
+  text: PropTypes.string.isRequired,
+ };
